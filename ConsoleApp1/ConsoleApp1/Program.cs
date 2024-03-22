cffc6c41efb2119ac017e1fb7186a5ad42f3df6f
s16896 <s16896@pjwstk.edu.pl>
2024-03-22 09:51:58 +0100

    find max function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,3 +10,20 @@ for (int a = 1; a <= 10; a++)
     Console.WriteLine(a);
 }
 Console.WriteLine("AGAIN Here we have the world");
+static int ZnajdzMax(int[] tablica)
+{
+    if (tablica == null || tablica.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta");
+    }
+
+    int maksymalnaWartosc = tablica[0];
+    foreach (int liczba in tablica)
+    {
+        if (liczba > maksymalnaWartosc)
+        {
+            maksymalnaWartosc = liczba;
+        }
+    }
+    return maksymalnaWartosc;
+}
