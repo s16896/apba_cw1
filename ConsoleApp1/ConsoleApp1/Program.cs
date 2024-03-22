07d11dd0b597bcdb8608d49eda34c299129a6baf
s16896 <s16896@pjwstk.edu.pl>
2024-03-22 05:09:49 +0100

    Modyfikacja 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("This time it's not the world");
 for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine(i);
 }
+Console.WriteLine("Here we have the world");
