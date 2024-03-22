d5e42bc1478b534db7c240dd493725217e046de8
s16896 <s16896@pjwstk.edu.pl>
2024-03-22 05:10:43 +0100

    Modyfikacja 3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,8 @@ for (int i = 1; i <= 10; i++)
     Console.WriteLine(i);
 }
 Console.WriteLine("Here we have the world");
+for (int a = 1; a <= 10; a++)
+{
+    Console.WriteLine(a);
+}
+Console.WriteLine("AGAIN Here we have the world");
